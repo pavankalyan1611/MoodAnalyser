53ed8f5218e526ff4284589dca4df340cda97f93
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-10 13:19:20 +0530

    [pavan] should return happy

--- MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
@@ -20,7 +20,21 @@ namespace MoodAnalyserTest
 
             //Assert
             Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void GivenAnyMoodMessage_WhenAnalyseShouldReturnHappy()
+        {
+            //Arrange
+            MoodAnalyser analyser = new MoodAnalyser();
+            string message = "I am in Any Mood";
+            string expected = "HAPPY";
+
+            //Act
+            string actual = analyser.analyseMood(message);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
     }
