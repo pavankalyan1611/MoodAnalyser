9d6f55183f2d3cbdd132b6ddd86e70c75d12bedd
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-12 18:47:41 +0530

    [pava] creating obj with parameter constructor using reflections

--- MoodAnalyser/MoodAnalyser/MoodAnalyserFactory.cs
+++ MoodAnalyser/MoodAnalyser/MoodAnalyserFactory.cs
@@ -41,5 +41,28 @@ namespace MoodAnalyserPbr
             }
             return null;
         }
+
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+            }
+        }
     }
 }
--- MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
@@ -7,17 +7,26 @@ namespace MoodAnalyserTest
     public class UnitTest1
     {
 
+        //[TestMethod]
+        //public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        //{
+        //    //Arrange
+        //    //string message ="";
+        //    string expected = "Constructor is not found";
+        //    //Act
+        //    string actual = (string) MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserPbr.MoodAnalyser", "MoodAnalyse");
+        //    //Assert
+        //       //expected.Equals(obj);
+        //    Assert.AreEqual(expected, actual);
+        //}
         [TestMethod]
-        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject_UsingParameterizedConstructor()
         {
-            //Arrange
-            //string message ="";
-            string expected = "Constructor is not found";
-            //Act
-            string actual = (string) MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserPbr.MoodAnalyser", "MoodAnalyse");
-            //Assert
-               //expected.Equals(obj);
-            Assert.AreEqual(expected, actual);
-        }    
+            object expected = new MoodAnalyser("HAPPY"); //2000
+           //MoodAnalyser obj = (MoodAnalyser)expected;           
+            object obj = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserPbr.MoodAnalyser", "MoodAnalyser", "HAPPY");
+            expected.Equals(obj);
+            //Assert.AreEqual(expected, obj);
+        }
     }
 }
